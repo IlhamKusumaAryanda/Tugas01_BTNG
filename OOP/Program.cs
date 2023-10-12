bbec8fd1293d81d47115f2bc04c53d5433609323
AryanIruma <115530462+IlhamKusumaAryanda@users.noreply.github.com>
2023-10-12 23:18:34 +0700

    Add files via upload

---
+++ OOP/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car kijang = new Car();
+            Car Ferrari = new Car();
+            Car Toyota = new Car();
+            kijang.setSpeed(200);
+            Ferrari.setSpeed(100);
+            Toyota.setMerk("Toyota");
+            //Audi.detailMobil();
+            
+        }
+    }
+
+    class Car
+    {
+        public string merk = "kijang";
+        
+        public void setMerk(string merkbaru)
+        {
+            this.merk = merkbaru;
+            Console.WriteLine(this.merk);
+        }
+        int Speed = 200;
+        public void setSpeed(int Speedbaru)
+        {
+            this.Speed = Speedbaru;
+            Console.WriteLine(this.Speed);
+        }
+        private void  privatemethod()
+        {
+            Console.WriteLine("Private Method");
+        }
+        public void publicMethod()
+        {
+            privatemethod();
+        }
+    }
+}
